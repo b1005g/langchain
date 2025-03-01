76fe16efa3756e9e5e84aa42b5b6c5fffe4f3cd0
b1005g <b1005g25@gmail.com>
2025-03-01 23:45:23 +0900

    Add RAG

--- react-langchain/main2.py
+++ react-langchain/main2.py
@@ -1,37 +1,34 @@
-from typing import Union, List
+from typing import List, Union
 from dotenv import load_dotenv
 from langchain.agents import tool
 from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.schema import AgentAction, AgentFinish
 from langchain.tools import Tool
 from langchain.tools.render import render_text_description
-
-from callbacks import AgentCallbackHandler
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
+
 @tool
-def get_text_length(text:str) -> int :
-    """
-    Returns the length of a text by characters
-    """
-    print(f"get_text_length enter with {text=}")
-    text = text.strip("'\n").strip('"')
+def get_text_length(text: str) -> int:
+    """주어진 문자열의 길이를 반환하는 함수"""
+    text = text.strip("'\n")
     return len(text)
 
-def find_tool_by_name(tools : List[Tool], tool_name:str) -> Tool:
+
+def find_tool(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
         if tool.name == tool_name:
             return tool
-    raise ValueError(f"Tool with name {tool_name} not found")
+    raise ValueError(f"{tool_name}을 가진 Tool을 찾을 수 없습니다.")
+
 
 if __name__ == "__main__":
-    print("Hello React!")
     tools = [get_text_length]
-    
+
     template = """
     Answer the following questions as best you can. You have access to the following tools:
 
@@ -53,30 +50,45 @@ if __name__ == "__main__":
     Question: {input}
     Thought: {agent_scratchpad}
     """
-    prompt = PromptTemplate.from_template(template=template).partial(
-        tools=render_text_description(tools), tool_names=", ".join([t.name for t in tools]),
-        )
-        #tools=tools, tool_name=", ".join([t.name for t in tools]))
-    
-    llm = ChatOpenAI(temperature=0, model_kwargs={"stop": ["\nObservation", "Observation"]}, callbacks=[AgentCallbackHandler()])
+
+    prompt = PromptTemplate.from_template(template)
+    prompt = prompt.partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join(t.name for t in tools),
+    )
+
+    llm = ChatOpenAI(
+        temperature=0,
+        model_kwargs={"stop": ["\nObservation", "Observation"]},
+    )
+
     intermediate_steps = []
 
-    agent = ({"input": lambda x: x["input"], 
-             "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),} | prompt | llm | ReActSingleInputOutputParser())
-    agent_step = ""
+    agent = prompt | llm | ReActSingleInputOutputParser()
+
+    # Invoke
+    agent_step = None
+
     while not isinstance(agent_step, AgentFinish):
-        agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input" : "'ReAct'의 길이는?", "agent_scratchpad" : intermediate_steps,})
+        agent_step = agent.invoke(
+            {
+                "input": "'ReAct'의 길이는?",
+                "agent_scratchpad": format_log_to_str(intermediate_steps),
+            }
+        )
+
         print(agent_step)
 
         if isinstance(agent_step, AgentAction):
             tool_name = agent_step.tool
-            tool_to_use = find_tool_by_name(tools, tool_name)
             tool_input = agent_step.tool_input
+            tool_to_use = find_tool(tools, tool_name)
+            observation = tool_to_use.invoke(tool_input)
 
-            observation = tool_to_use.func(str(tool_input))
             print(f"{observation=}")
+
             intermediate_steps.append((agent_step, str(observation)))
-    
-    '''agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input" : "'ReAct'의 길이는?", "agent_scratchpad" : intermediate_steps,})'''
-    if isinstance(agent_step, AgentFinish):
-        print(agent_step.return_values)
+
+        if isinstance(agent_step, AgentFinish):
+            print("=== Agent Finish!!===")
+            print(agent_step.return_values)
